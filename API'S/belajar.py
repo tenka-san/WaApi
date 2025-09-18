5a10b6045af50beede395701d81fb5cf07567e3a
D | Yuukey-7eppeli <minikrap2@gmail.com>
2025-09-18 15:09:03 +0700

    Add files via upload

---
+++ API'S/belajar.py
@@ -0,0 +1,7 @@
+import math
+
+pertama = int(input('masukan nomor pertama\n'))
+kedua = int(input('masukan nomor kedua\n'))
+hasil = math.floor(pertama + kedua)
+
+print(f'\nhasil dari {pertama} + {kedua} adalah {hasil}')
