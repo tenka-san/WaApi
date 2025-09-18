965e6747e7103b08e4853eb9181fb3e9451216c8
D | Yuukey-7eppeli <minikrap2@gmail.com>
2025-09-18 15:17:47 +0700

    Delete API'S directory

--- API'S/belajar.py
+++
@@ -1,7 +0,0 @@
-import math
-
-pertama = int(input('masukan nomor pertama\n'))
-kedua = int(input('masukan nomor kedua\n'))
-hasil = math.floor(pertama + kedua)
-
-print(f'\nhasil dari {pertama} + {kedua} adalah {hasil}')
